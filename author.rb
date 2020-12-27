f21cd23bad5ffec7b1df4643e7dbace698345ecb
Miguel Savignano <migue.masx@gmail.com>
2020-12-28 00:13:55 +0100

    type block

--- author.rb
+++ author.rb
@@ -2,4 +2,12 @@ class Author
   def initialize(name)
     @name = name
   end
+
+  def with_name(&block)
+    yield @name
+  end
 end
+
+# Author.new("name").with_name do |name|
+#   puts name
+# end
